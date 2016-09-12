49fc271a5e126bd4cb1296cd5ab6b91e3bc89077
Gabriella Castaneda <gabriellacastaneda@gmail.com>
2016-09-12 22:41:47 +0000

    initial commit

---
+++ server.js
@@ -0,0 +1,6 @@
+var express = require('express'),
+    app = express(),
+    mongoose = require('mongoose'),
+    morgan = require('morgan'),
+    bodyParser = require('body-parser'),
+    methodOverride = require('method-override')
