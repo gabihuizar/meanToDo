513addd24bb07f65c2554a798f32178ca5b251ab
Gabriella Castaneda <gabriellacastaneda@gmail.com>
2016-09-13 16:32:13 +0000

    changed mongo connect uri

--- server.js
+++ server.js
@@ -4,3 +4,16 @@ var express = require('express'),
     morgan = require('morgan'),
     bodyParser = require('body-parser'),
     methodOverride = require('method-override')
+
+mongoose.connect('mongodb://gaby:mean@jello.modulusmongo.net:27017/R7idatyx');
+
+
+app.use(express.static(__dirname + '/public'));
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({'extended':'true'}));
+app.use(bodyParser.json());
+app.use(bodyParser.json({ type: 'application/vnd.api+json '}));
+app.use(methodOverride());
+
+app.listen(8080);
+console.log("App listening on port 8080");
